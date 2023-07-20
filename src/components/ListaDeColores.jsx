1270d81941aca0bcefb82e543f977912d3b0ecbd
¨mago¨ <martingramajo08@gmail.com>
2023-07-20 20:37:14 -0300

    agrego  component ListaDeColores

---
+++ src/components/ListaDeColores.jsx
@@ -0,0 +1,11 @@
+import ItemCardColor from "./ItemCardColor";
+
+const ListaDeColores = () => {
+  return (
+    <section className="container my-5 d-flex justify-content-between">
+      <ItemCardColor />
+    </section>
+  );
+};
+
+export default ListaDeColores;
