7c13333dfaa95ad47557f5e73b0032936609fabd
¨mago¨ <martingramajo08@gmail.com>
2023-07-20 20:29:59 -0300

    agrego los state, el guardado en local storage y las funciones handleChange handleSubmit en FormulariosColores

--- src/components/FormularioColores.jsx
+++ src/components/FormularioColores.jsx
@@ -1,25 +1,50 @@
+import { useEffect, useState } from "react";
 import { Button, Card, Container, Form, InputGroup } from "react-bootstrap";
+let coloresIniciales = JSON.parse(localStorage.getItem("colores")) || [];
 
 const FormularioColores = () => {
+  const [color, setColor] = useState("");
+  const [listaColores, setListaColores] = useState(coloresIniciales);
+
+  useEffect(() => {
+    if (coloresIniciales) {
+      localStorage.setItem("colores", JSON.stringify(listaColores));
+    } else {
+      localStorage.setItem("colores", JSON.stringify([]));
+    }
+  }, [listaColores]);
+
+  const handleChange = (e) => {
+    setColor(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setListaColores([...listaColores, color]);
+    setColor("");
+  };
+
   return (
     <Container>
       <Card>
         <Card.Header className="py-4">Administrar Colores</Card.Header>
         <Card.Body>
-          <Form className="fondo-formulario">
+          <Form className="fondo-formulario" onSubmit={handleSubmit}>
             <div className="d-flex justify-content-between py-5  ">
               <div
+                className="color-box"
                 style={{
-                  backgroundColor: `red`,
-                  width: "130px",
-                  height: "90px",
-                  marginRight: "20px",
-                  marginLeft: "20px",
+                  backgroundColor: `${color}`,
                 }}
               ></div>
 
               <InputGroup className="mb-3 me-5 py-1">
-                <Form.Control placeholder="Ingrese una Tarea" type="text" />
+                <Form.Control
+                  placeholder="Ingrese un color ej blue"
+                  type="text"
+                  value={color}
+                  onChange={handleChange}
+                />
               </InputGroup>
             </div>
             <Card.Footer className="d-flex  justify-content-end pt-4 bg-white ">
